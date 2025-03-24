dd9602df1abebf53a2686f7ffe55ed0d6c3a940c
disinel <disine@gmail.com>
2025-03-24 18:05:25 +0100

    Add files via upload

---
+++ Figs_27_28/plot_Fig27_upper_panel.m
@@ -0,0 +1,38 @@
+clear variables;
+close all;
+
+% Number of points
+N=10000;
+
+
+EventVar=cell(N*2,1);
+EventVar(:)={'DECEASED'};
+
+TreatVar=cell(N*2,1);
+TreatVar(1:N)={'2C10T'};
+TreatVar(N+1:2*N)={'10T'};
+
+% Loading data from the file 'CAR-T_TMZ_Tsurv_max_r1_0_025_rho4_10_v_5_N=10000'
+load('CAR-T_TMZ_Tsurv_max_r1_0_025_rho4_10_v_5_N=10000');
+
+Tstct=Tsurv';
+
+
+
+% Loading data from the file 'TMZ_only_Tsurv_max_r1_0_025_rho4_10_N=10000'
+load('TMZ_only_Tsurv_max_r1_0_025_rho4_10_N=10000');
+
+Tstmz=Tsurv';
+
+
+times=[Tstct;Tstmz];
+
+[p,fh,stats]=MatSurv(times, EventVar,  TreatVar,'TimeMax',2000,'DispP',false,'DispHR',false,'Xlabel','t');
+
+
+
+
+
+
+
+
---
+++ Figs_31_32/plot_Fig31_upper_panel.m
@@ -0,0 +1,38 @@
+clear variables;
+close all;
+
+% Number of points
+N=10000;
+
+
+EventVar=cell(N*2,1);
+EventVar(:)={'DECEASED'};
+
+TreatVar=cell(N*2,1);
+TreatVar(1:N)={'10T2C'};
+TreatVar(N+1:2*N)={'10T'};
+
+% Loading data from the file 'TMZ_CAR-T_Tsurv_max_r1_0_025_rho4_10_v_5_N=10000'
+load('TMZ_CAR-T_Tsurv_max_r1_0_025_rho4_10_v_5_N=10000');
+
+Tstct=Tsurv';
+
+
+
+% Loading data from the file 'TMZ_only_Tsurv_max_r1_0_025_rho4_10_N=10000'
+load('TMZ_only_Tsurv_max_r1_0_025_rho4_10_N=10000');
+
+Tstmz=Tsurv';
+
+
+times=[Tstct;Tstmz];
+
+[p,fh,stats]=MatSurv(times, EventVar,  TreatVar,'TimeMax',2000,'DispP',false,'DispHR',false,'Xlabel','t');
+
+
+
+
+
+
+
+
